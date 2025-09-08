c77f719943b8f2ac122ecd87412b4374441eb17c
Rafiza-Fried-Chicken <mangvios0@gmail.com>
2025-09-08 21:21:02 +0700

    Update and rename setting.php to config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+export const credentials = {
+  username: "javabestT",
+  password: "javabestT",
+};
+
+export const urls = [
+  "https://example.com/1",
+  "https://example.com/2",
+  "https://example.com/3",
+  "https://example.com/4",
+  "https://example.com/5",
+];
+
+export const whatsappLink = "https://wa.me/1234567890"; // ganti dengan nomor WA Kit Anda
